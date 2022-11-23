0cb7a476a072025b64d42fabc61f46b48ffa1151
mehulbhange <bhangemehul@gmail.com>
2022-11-23 18:05:05 +0530

    ADDED:[Mehul] ability to update employee payroll service

--- src/components/payroll-form/PayrollForm.jsx
+++ src/components/payroll-form/PayrollForm.jsx
@@ -6,12 +6,13 @@ import profile7 from '../../assets/images/Ellipse -7.png'
 import profile8 from '../../assets/images/Ellipse -8.png'
 import EmployeeService from '../../service/EmployeeService';
 
+
 class PayrollForm extends Component {
 
     
     constructor(props){
         super(props);
-        
+
         this.state = {
             name:"",
             profilePic:"",
@@ -24,6 +25,38 @@ class PayrollForm extends Component {
             year:"",
             submitMsg:"",
             nameError:"",
+            isUpdate: false,
+        }   
+    }
+
+    getEmployeeById = (empId) => {
+        EmployeeService.getEmployeeById(empId).then( (response) => {
+            console.log("get emp by id called")
+            let date = response.data.data.startDate.split('-');
+            //{year, month, day } = response.data.data.startDate.slice('-');
+            this.setState({
+                name:response.data.data.name,
+                profilePic:response.data.data.profilePic,
+                gender:response.data.data.gender,
+                departments: response.data.data.departments,
+                salary: response.data.data.salary,
+                notes: response.data.data.note,
+                day:date[2],
+                month:date[1],
+                year:date[0],
+                
+            })
+            
+            console.log(this.state.departments)
+        })
+    }
+
+    componentDidMount(){
+        if(this.props.location.state){
+            this.getEmployeeById(this.props.location.state.employeeId);
+            this.setState({
+                isUpdate:true,
+            })
         }
     }
 
@@ -39,33 +72,59 @@ class PayrollForm extends Component {
             note : this.state.notes,            
         } 
         
-        //console.log("Employee object "+ empObject)
-        console.log("Employee object "+JSON. stringify(empObject));
-
-        EmployeeService.addEmployee(empObject).then( (response) =>{
-            this.setState({
-                submitMsg: "Employee added successfully"
-            })
-            setInterval(() => {
+        if( this.state.isUpdate){
+            EmployeeService.updateEmployee(this.props.location.state.employeeId, empObject).then(() =>{
                 this.setState({
-                    submitMsg: ""
-                })    
-            },10000)
-        }).catch( (error) =>{
-            this.setState({
-                submitMsg: "error Something went wrong! Record not added"
-            })
-            setInterval(() => {
+                    submitMsg: "Employee updated successfully"
+                })     
+                setInterval(() => {
+                    this.setState({
+                        submitMsg: ""
+                    })    
+                },10000)
+                
+            }).catch( () =>{
                 this.setState({
-                    submitMsg: ""
+                    submitMsg: "error Something went wrong! Record not updated"
                 })    
-            },10000)
-        })
+                setInterval(() => {
+                    this.setState({
+                        submitMsg: ""
+                    })    
+                },10000)    
+            })
+        }else{
+            EmployeeService.addEmployee(empObject).then( (response) =>{
 
+                this.setState({
+                    submitMsg: "Employee added successfully"
+                })
+                
+                setInterval(() => {
+                    this.setState({
+                        submitMsg: ""
+                    })    
+                },10000)
+            }).catch( (error) =>{
+                
+                this.setState({
+                    submitMsg: "error Something went wrong! Record not added"
+                })
+    
+                console.log(error)
+    
+                setInterval(() => {
+                    this.setState({
+                        submitMsg: ""
+                    })    
+                },10000)
+            })
+        }
         
     }
 
     onValueChange = (event) => {
+        
         const nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
         if( event.target.name == "name"){
             if(nameRegex.test(event.target.value)){
@@ -132,7 +191,7 @@ class PayrollForm extends Component {
 
                         <div className="row-content">
                             <label className="label text" htmlFor="name"> Name : </label>
-                            <input className="input" onChange={this.onValueChange} type="text" name="name" id="name" placeholder="Your name." required />
+                            <input className="input" onChange={this.onValueChange} value={this.state.name} type="text" name="name" id="name" placeholder="Your name." required />
                             <span className='error-output'> {this.state.nameError} </span>
                         </div>
 
@@ -140,19 +199,19 @@ class PayrollForm extends Component {
                             <label htmlFor="profile" className="label text">Profile image</label>
                             <div className="profile-radio-content">
                                 <label>
-                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile1" value="../../assets/images/Ellipse -3.png" required />
+                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile1" value="../../assets/images/Ellipse -3.png" checked={this.state.profilePic == "../../assets/images/Ellipse -3.png" } required />
                                     <img src={profile3} className="profile" alt="" />
                                 </label>
                                 <label>
-                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile2" value="../../assets/images/Ellipse 1.png" required />
+                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile2" value="../../assets/images/Ellipse 1.png" checked={this.state.profilePic == "../../assets/images/Ellipse 1.png"} required />
                                     <img src={profile1} className="profile" alt="" />
                                 </label>
                                 <label>
-                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile3" value="../../assets/images/Ellipse -8.png" required />
+                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile3" value="../../assets/images/Ellipse -8.png" checked={this.state.profilePic == "../../assets/images/Ellipse -8.png"} required />
                                     <img src={profile8} className="profile" alt="" />
                                 </label>
                                 <label>
-                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile4" value="../../assets/images/Ellipse -7.png" required />
+                                    <input type="radio" onChange={this.onValueChange} name="profilePic" id="profile4" value="../../assets/images/Ellipse -7.png" checked={this.state.profilePic == "../../assets/images/Ellipse -7.png"} required />
                                     <img src={profile7} className="profile" alt="" />
                                 </label>
                             </div>
@@ -161,9 +220,9 @@ class PayrollForm extends Component {
                         <div className="row-content">
                             <label htmlFor="gender" className="label text">Gender</label>
                             <div>
-                                <input type="radio" onChange={this.onValueChange} name="gender" id="male" value="male" required />
+                                <input type="radio" onChange={this.onValueChange} checked={this.state.gender == "male"} name="gender" id="male" value="male" required />
                                 <label htmlFor="male" className="text">Male</label>
-                                <input type="radio" onChange={this.onValueChange} name="gender" id="female" value="female" required/>
+                                <input type="radio" onChange={this.onValueChange} checked={this.state.gender == "female"} name="gender" id="female" value="female" required/>
                                 <label htmlFor="female" className="text">Female</label>
                             </div>
                         </div>
@@ -171,15 +230,15 @@ class PayrollForm extends Component {
                         <div className="row-content">
                             <label htmlFor="department" name="department" className="label text">Department</label>
                             <div>
-                                <input type="checkbox" onChange={this.onCheckboxChange} className="checkbox" name="hr" id="hr" value="HR" />
+                                <input type="checkbox" checked={this.state.departments.includes("HR")} onChange={this.onCheckboxChange} className="checkbox" name="hr" id="hr" value="HR" />
                                 <label htmlFor="hr" className="text">HR</label>   
-                                <input type="checkbox" onChange={this.onCheckboxChange} className="checkbox" name="sales" id="sales" value="Sales" />
+                                <input type="checkbox" checked={this.state.departments.includes("Sales")} onChange={this.onCheckboxChange} className="checkbox" name="sales" id="sales" value="Sales" />
                                 <label htmlFor="sales" className="text">Sales</label>
-                                <input type="checkbox" onChange={this.onCheckboxChange} className="checkbox" name="finance" id="finance" value="Finance" />
+                                <input type="checkbox" checked={this.state.departments.includes("Finance")} onChange={this.onCheckboxChange} className="checkbox" name="finance" id="finance" value="Finance" />
                                 <label htmlFor="finance" className="text">Finance</label>
-                                <input type="checkbox" onChange={this.onCheckboxChange} className="checkbox" name="engineer" id="engineer" value="Engineer" />
+                                <input type="checkbox" checked={this.state.departments.includes("Engineer")} onChange={this.onCheckboxChange} className="checkbox" name="engineer" id="engineer" value="Engineer" />
                                 <label htmlFor="engineer" className="text">Engineer</label> 
-                                <input type="checkbox" onChange={this.onCheckboxChange} className="checkbox" name="others" id="others" value="Others" />
+                                <input type="checkbox" checked={this.state.departments.includes("Others")} onChange={this.onCheckboxChange} className="checkbox" name="others" id="others" value="Others" />
                                 <label htmlFor="others" className="text">Others</label>            
                             </div>
                         </div>
@@ -193,7 +252,7 @@ class PayrollForm extends Component {
                         <div className="row-content">
                             <label htmlFor="startDate" className="label text">Start Date</label>
                             <div onChange={this.onValueChange}>
-                                <select name="day" id="day" >
+                                <select name="day" id="day" value={this.state.day}>
                                     <option>Day</option>
                                     <option value="01">1</option>
                                     <option value="02">2</option>
@@ -227,22 +286,22 @@ class PayrollForm extends Component {
                                     <option value="30">30</option>
                                     <option value="31">31</option>
                                 </select>
-                                <select name="month" id="month" >
+                                <select name="month" id="month" value={this.state.month} >
                                     <option>Month</option>
-                                    <option value="0">January</option>
-                                    <option value="01">February</option>
-                                    <option value="02">March</option>
-                                    <option value="03">April</option>
-                                    <option value="04">May</option>
-                                    <option value="05">June</option>
-                                    <option value="06">July</option>
-                                    <option value="07">August</option>
-                                    <option value="08">September</option>
-                                    <option value="09">October</option>
-                                    <option value="10">November</option>
-                                    <option value="11">December</option>
+                                    <option value="01">January</option>
+                                    <option value="02">February</option>
+                                    <option value="03">March</option>
+                                    <option value="04">April</option>
+                                    <option value="05">May</option>
+                                    <option value="06">June</option>
+                                    <option value="07">July</option>
+                                    <option value="08">August</option>
+                                    <option value="09">September</option>
+                                    <option value="10">October</option>
+                                    <option value="11">November</option>
+                                    <option value="12">December</option>
                                 </select>
-                                <select name="year" id="year" >
+                                <select name="year" id="year" value={this.state.year}>
                                     <option>Year</option>
                                     <option value="2022">2022</option>
                                     <option value="2021">2021</option>
@@ -255,7 +314,7 @@ class PayrollForm extends Component {
 
                         <div className="row-content">
                             <label htmlFor="notes" className="label text">Notes</label>
-                            <textarea name="notes" id="notes" className="input" onChange={this.onValueChange} required></textarea>
+                            <textarea name="notes" id="notes" className="input" onChange={this.onValueChange} value={this.state.notes} required></textarea>
                         </div>
 
                         <div>
--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -3,12 +3,24 @@ import axios from 'axios';
 class EmployeeService{
     baseUrl ="http://localhost:8081/employeepayrollservice";
 
-    async addEmployee(data) {
+    addEmployee(data) {
         return axios.post(`${this.baseUrl}/create`, data);
     }
 
     getAllEmployees() {
         return axios.get(`${this.baseUrl}/get`);
+    }
+
+    getEmployeeById(employeeId) {
+        return axios.get(`${this.baseUrl}/get/${employeeId}`);
+    }
+
+    updateEmployee(employeeId,data) {
+        return axios.put(`${this.baseUrl}/update/${employeeId}`, data);
+    }
+
+    deleteEmployee(employeeId) {
+        return axios.delete(`${this.baseUrl}/delete/${employeeId}`);
     }
 }
 
